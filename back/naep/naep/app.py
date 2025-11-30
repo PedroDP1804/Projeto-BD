cc242298b779ac460527404c6c3371a8df82b47d
Lucas <lucaspework@gmail.com>
2025-11-30 16:32:05 -0300

    feat: update user to pesquiador + create, update, read pesquisador and pesquisa

--- back/naep/naep/app.py
+++ back/naep/naep/app.py
@@ -2,44 +2,92 @@ from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
 
-from naep.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from naep.schemas import (
+    Message,
+    PesquisaDB,
+    PesquisadorDB,
+    PesquisadorList,
+    PesquisadorPublic,
+    PesquisadorSchema,
+    PesquisaList,
+    PesquisaPublic,
+    PesquisaSchema,
+)
 
 app = FastAPI(title='NAEP - API')
-database = []
+db_pesquisadores = []
+db_pesquisas = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'test'}
 
 
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        username=user.username,
-        email=user.email,
-        password=user.password,
+@app.post('/pesquisadores/', status_code=HTTPStatus.CREATED, response_model=PesquisadorPublic)
+def create_pesquisador(pesquisador: PesquisadorSchema):
+    pesquisador_with_id = PesquisadorDB(
+        id=len(db_pesquisadores) + 1,
+        **pesquisador.model_dump()
     )
 
-    database.append(user_with_id)
-
-    return user_with_id
-
-
-@app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': database}
+    db_pesquisadores.append(pesquisador_with_id)
+    return pesquisador_with_id
 
 
-@app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+@app.get('/pesquisadores/', response_model=PesquisadorList)
+def read_pesquisadores():
+    return {'pesquisadores': db_pesquisadores}
+
+
+@app.get('/pesquisadores/{pesquisador_id}', status_code=HTTPStatus.OK, response_model=PesquisadorPublic)
+def read_pesquisador_by_id(pesquisador_id: int):
+    if pesquisador_id > len(db_pesquisadores) or pesquisador_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Pesquisador not found')
+
+    return db_pesquisadores[pesquisador_id - 1]
+
+
+@app.put('/pesquisadores/{pesquisador_id}', response_model=PesquisadorPublic)
+def update_pesquisador(pesquisador_id: int, pesquisador: PesquisadorSchema):
+    if pesquisador_id > len(db_pesquisadores) or pesquisador_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+            status_code=HTTPStatus.NOT_FOUND, detail='Pesquisador not found'
         )
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    pesquisador_with_id = PesquisadorDB(**pesquisador.model_dump(), id=pesquisador_id)
+    db_pesquisadores[pesquisador_id - 1] = pesquisador_with_id
 
-    return user_with_id
+    return pesquisador_with_id
+
+
+@app.post('/pesquisas/', status_code=HTTPStatus.CREATED, response_model=PesquisaPublic)
+def create_pesquisa(pesquisa: PesquisaSchema):
+    if pesquisa.id_pesquisador_responsavel > len(db_pesquisadores) or pesquisa.id_pesquisador_responsavel < 1:
+         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Pesquisador ID invalid')
+
+    pesquisa_with_id = PesquisaDB(
+        id=len(db_pesquisas) + 1,
+        **pesquisa.model_dump()
+    )
+
+    db_pesquisas.append(pesquisa_with_id)
+    return pesquisa_with_id
+
+
+@app.get('/pesquisas/', response_model=PesquisaList)
+def read_pesquisas():
+    return {'pesquisas': db_pesquisas}
+
+
+@app.put('/pesquisas/{pesquisa_id}', response_model=PesquisaPublic)
+def update_pesquisa(pesquisa_id: int, pesquisa: PesquisaSchema):
+    if pesquisa_id > len(db_pesquisas) or pesquisa_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='Pesquisa not found'
+        )
+
+    pesquisa_with_id = PesquisaDB(**pesquisa.model_dump(), id=pesquisa_id)
+    db_pesquisas[pesquisa_id - 1] = pesquisa_with_id
+
+    return pesquisa_with_id
